00502a8ea6450facddfc33fb7ce45c833052f57f
VictorLorenzo <victor2003lorenzo@gmail.com>
2025-04-16 21:55:23 -0400

    feat: add spark classes to run script

--- airflow/spark/nba_raw_to_bronze_processor.py
+++ airflow/spark/nba_raw_to_bronze_processor.py
@@ -3,26 +3,36 @@ import logging
 
 # Create Spark Session
 def main():
-    app_name = 'nba_raw_to_silver_processor'
+    app_name = "nba_raw_to_silver_processor"
     config_file = {
-        'spark_options': {
-            'source': {
-                'format': 'csv',
-                'options':{
-                    'header': 'true',
-                    'delimiter': ',',
-                    'inferSchema': 'true'
+        "type_processing": "batch",
+        "spark_options": {
+            "source": {
+                "format": "csv",
+                "options":{
+                    "header": "true",
+                    "delimiter": ",",
+                    "inferSchema": "true"
                 },
-                'process_type': 'file',
-                'path':'s3a://datalake-landing/datalake-raw-nba/nba'
+                "process_type": "file",
+                "path":"s3a://datalake-landing/datalake-raw-nba/nba"
             },
-            'target': {
-                'format': 'delta',
-                'options':{
-                    'checkpoint': 's3a://datalake-bronze/datalake-bronze-nba/_checkpoints'
+            "target": {
+                "database": "nba_bronze",
+                "table": "nba",
+                "format": "delta",
+                "options":{
+                    "checkpoint": "s3a://datalake-bronze/datalake-bronze-nba/_checkpoints",
+                    "output_mode": "append",
+                    "apply_as_delete": None
                 },
-                'path':'s3a://datalake-bronze/datalake-bronze-nba/nba',
-                'mode': 'append'
+                "path":"s3a://datalake-bronze/datalake-bronze-nba/nba",
+                "primary_key": ["PLAYER_ID", "SECS_LEFT", "MINS_LEFT", "GAME_ID"],
+                "sequence_by": ["_bronze_created_at"],
+                "sql_transform":{
+                    "10-add-layser-columns": ["input_file_name() _filename", "current_timestamp() _bronze_created_at"],
+                },
+                "drop_columns": []
             }
         }
     }
--- airflow/spark/utils/spark_utils.py
+++ airflow/spark/utils/spark_utils.py
@@ -1,5 +1,7 @@
 import random
 from pyspark.sql import SparkSession
+from delta.tables import DeltaTable
+
 import logging
 
 class SparkUtils():
@@ -26,34 +28,6 @@ class ProcessData():
         logging.basicConfig(level=logging.INFO)
         self.logger = logging.getLogger(__name__)
 
-    def process_file(self):
-        df = self._read_source_file()
-        writer = self._build_writer(df)
-        #self.execute_process_streaming(writer)
-
-    def _build_writer(self, df):
-        process_config = self.process_config
-        process_config = process_config["type_processing"]
-        if df.isStreaming:
-            writer = (
-                df.writeStream
-                .format(process_config["spark_options"]["target"]["format"])
-                .option("path", process_config["spark_options"]["target"]["path"])
-                .option("checkpointLocation", process_config["spark_options"]["target"]["options"]["checkpoint"])
-                .outputMode(process_config["spark_options"]["target"]["options"]["output_mode"])
-                .foreachBatch(self._foreach_batch)
-                .queryName(f"writeing table into deltalake")
-            )
-
-    def _foreach_batch(self, df, batch_id):
-
-    def execute_process_streaming(self,writer):
-        writer = writer.trigger(
-            availableNow=True
-        )
-
-        writer.start().awaitTermination()
-
     def execute(self):
         logger = self.logger
         process_config = self.process_config
@@ -66,6 +40,11 @@ class ProcessData():
             logger.error("An error ocurred: %s", str(e))
             logger.info("Data processing finished with errors.")
             raise e
+
+    def process_file(self):
+        df = self._read_source_file()
+        writer = self._build_writer(df)
+        self.execute_process_streaming(writer)
 
     def _read_source_file(self):
         spark = self.spark
@@ -96,3 +75,165 @@ class ProcessData():
             )
 
         return df
+
+    def _build_writer(self, df):
+        process_config = self.process_config
+        type_processing = process_config["type_processing"]
+
+        if df.isStreaming:
+            writer = (
+                df.writeStream
+                .format(process_config["spark_options"]["target"]["format"])
+                .option("path", process_config["spark_options"]["target"]["path"])
+                .option("checkpointLocation", process_config["spark_options"]["target"]["options"]["checkpoint"])
+                .outputMode(process_config["spark_options"]["target"]["options"]["output_mode"])
+                .foreachBatch(self._foreach_batch)
+                .queryName(f"writing_{type_processing}_{process_config['spark_options']['target']['table']}")
+            )
+
+            return writer
+
+    def _foreach_batch(self, df, batch_id):
+        logger = self.logger
+
+        logger.info(f"Batch ID: {batch_id}")
+
+        source = "source_"
+        target = "target_"
+
+        self._create_database()
+
+        df = self._add_collums(df)
+
+        df = self._drop_columns(df)
+
+        table = self._create_table(df.schema)
+
+        self.merge_data(table, df, source, target)
+
+    def merge_data(self, table, df, source, target):
+        logger = self.logger
+        process_config = self.process_config
+        primary_key = process_config["spark_options"]["target"]["primary_key"]   
+        merge_condition = "\n AND ".join([f"{source}.{key} = {target}.{key}" for key in primary_key]) if primary_key else "1 = 0"
+        sequence_by = process_config["spark_options"]["target"]["sequence_by"]
+        sequence_by_condition = "\n AND ".join([f"{source}.{key} >= {target}.{key}" for key in sequence_by]) if sequence_by else "1 = 0"
+        appply_as_delete = process_config["spark_options"]["target"]["options"]["apply_as_delete"]
+
+        logger.info("Delta table merge started.")
+
+        logger.info(f"""   
+                    MERGE INTO {table} AS {target}
+                    USING {df} AS {source}
+                    ON {merge_condition}
+                    WHEN MATCHED AND {sequence_by_condition} THEN
+                        UPDATE SET *
+                    WHEN NOT MATCHED THEN  
+                    """)
+        print("sequence by condition: ",sequence_by_condition)
+        try:
+            merge = (
+                table.alias(target)
+                .merge(
+                    df.alias(source),
+                    merge_condition
+                )
+            )
+
+            if sequence_by:
+                merge = merge.whenMatchedUpdateAll(
+                    condition=sequence_by_condition)
+            
+            else:
+                merge = merge.whenMatchedUpdateAll()
+
+            if appply_as_delete:
+                merge = merge.whenMatchedDelete(
+                    condition=appply_as_delete
+                )
+
+            merge = merge.whenNotMatchedInsertAll()
+            merge.execute()
+            logger.info("Delta table merge completed successfully.")
+        except Exception as e:
+            logger.error(f"Error during Delta table merge: {str(e)}")
+            raise e
+
+    def _create_table(self, schema):
+        spark = self.spark
+        logger = self.logger
+        process_config = self.process_config
+        db_name = process_config["spark_options"]["target"]["database"]
+        table_name = process_config["spark_options"]["target"]["table"]
+        table_path = process_config["spark_options"]["target"]["path"]
+        table_full_name = f"{db_name}.{table_name}"
+
+        try:
+            if not DeltaTable.isDeltaTable(spark, table_full_name):
+                delta_table_build = (
+                    DeltaTable.createIfNotExists(spark)
+                    .tableName(table_full_name)
+                    .location(table_path)
+                    .comment("Table created by Delta Lake")
+                    .addColumns(schema)
+                )
+                delta_table = delta_table_build.execute()
+                logger.info(f"Table {table_full_name} created successfully.")
+                return delta_table
+            else:
+                logger.info(f"Table {table_full_name} already exists.")
+                return DeltaTable.forPath(spark, table_full_name, table_path)
+        except Exception as e:
+            logger.error(f"Error creating table {table_full_name}: {str(e)}")
+            raise e
+
+    def _drop_columns(self, df):
+        logger = self.logger
+        process_config = self.process_config
+        drop_columns = process_config["spark_options"]["target"]["drop_columns"]
+
+        if drop_columns:
+            df = df.drop(*drop_columns)
+
+        return df
+
+    def _create_database(self):
+        spark = self.spark
+        logger = self.logger
+        process_config = self.process_config
+        db_name = process_config["spark_options"]["target"]["database"]
+        db_path = process_config["spark_options"]["target"]["path"]
+
+        try:
+            spark.sql(f"CREATE DATABASE IF NOT EXISTS {db_name} LOCATION '{db_path}'")
+            logger.info(f"Database {db_name} created successfully.")
+        except Exception as e:
+            logger.error(f"Error creating database {db_name}: {str(e)}")
+            raise e
+
+    def _add_collums(self, df):
+        logger = self.logger
+        process_config = self.process_config
+        sql_transform = process_config["spark_options"]["target"]["sql_transform"]
+
+        try:
+            for column, value in sorted(sql_transform.items()):
+                logger.info(f"Adding column {column} with value {value}")
+                df = df.selectExpr("*", *value)
+            logger.info("Columns added successfully.")
+            return df
+        except Exception as e:
+            logger.error(f"Error adding columns: {str(e)}")
+            raise e
+
+    def execute_process_streaming(self,writer):
+        typer_processing = self.process_config["type_processing"]
+        if writer:
+            if typer_processing == "streaming":
+                writer = writer.trigger(processingTime="30 seconds")
+            else:
+                writer = writer.trigger(
+                    availableNow=True
+                )
+            
+            writer.start().awaitTermination()
